e12eb74c4eff622a1e013673c23bb363f353c082
Björn Harrtell <bjorn@septima.dk>
2024-12-19 10:08:44 +0100

    Add typedoc

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,6 @@
 export { definePiniaJsonApiStore, Model, model, hasMany, belongsTo } from './pinia-json-api'
 export type { PiniaApiStoreDefinition, PiniaJsonApiStoreConfig, FindOptions } from './pinia-json-api'
-export type { JsonApiFetcher, FetchOptions } from './json-api-fetcher'
+export type { JsonApiFetcher, FetchOptions, PageOption } from './json-api-fetcher'
 export type {
   JsonApiDocument,
   JsonApiResource,
